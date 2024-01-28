7644304ff23f4e9ce9964fac2fb5659018d82d0f
anieszt <nilsbohr1994@gmail.com>
2024-01-28 21:06:03 +0100

    commit

--- control.vhd
+++ control.vhd
@@ -6,7 +6,7 @@ use work.package1.all;
 entity control is 
 
     port(
-        --hi_lvl_ctrl : in std logic
+        up_ctrl : in std_logic;
         clk, rst: in std_logic;
         V : out L_array;
         w_rom_read : out natural
@@ -16,26 +16,52 @@ end control;
 
 architecture rtl of control is
 
-    type state is (idle, stage1, stage2, stage3);
+    type state is (idle, load, stage1, stage2, stage3);
     signal state_reg, state_nxt : state;
-    signal current_stage : integer := 0;
+    signal c_stage : natural := 0;
 
     signal V_reg : L_array;
     signal V_nxt : L_array;
 
 begin
-    
+    -- control path
     process(clk, rst)
     begin
         if rst='1' then
             state_reg <= idle;
-        elsif rising_edge(clk) then
+        elsif rising_edge(clk) then --elsif *time for each stage*
             state_reg <= state_nxt;
         end if;
     end process;
 
-    --process(state_reg, rst)
-    --begin
-        
+    process(state_reg, rst)
+    begin      
+        case state_reg is
+            when idle => 
+                if up_ctrl = '1' then
+                    state_nxt <= load;
+                else
+                    state_nxt <= idle;
+                end if;
+            when load => 
+                state_nxt <= stage1;
+            when stage1 => 
+                state_nxt <= stage2;
+            when stage2 => 
+                state_nxt <= stage3;
+            when stage3 =>
+                state_nxt <= idle;
+        end case;                   
+    end process;
+
+    --data path
+
+    process(clk,rst)
+    begin
+        if rst = '1' then
+        V_reg <= (others => '0');
+        end if;
+    end process;
+
     
 end architecture rtl;
